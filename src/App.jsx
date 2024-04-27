6dd4a3bc8f75f18eb054b4f443a70ebff0001f93
Aditya Panchal <adipanchal443@gmail.com>
2024-04-28 00:52:51 +0530

    fixed-css

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-const [color,setColor] = useState("red");
+  const [color, setColor] = useState("red");
+
   return (
-    <div className='container' style={{backgroundColor: color}}>
-      <button onClick={()=> setColor("red")}>Red</button>      
-      <button onClick={()=> setColor("Green")}>Green</button>      
-      <button onClick={()=> setColor("Blue")}>Blue</button>      
+    <div className='container' style={{ backgroundColor: color }}>
+      <div className="btnCon">
+        <button className='cBtn' style={{ backgroundColor: 'red' }} onClick={() => setColor("red")}>Red</button>
+        <button className='cBtn' style={{ backgroundColor: 'green' }} onClick={() => setColor("green")}>Green</button>
+        <button className='cBtn' style={{ backgroundColor: 'blue' }} onClick={() => setColor("blue")}>Blue</button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
